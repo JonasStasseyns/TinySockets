d4a97af0e60435b3151b5734017e89261b083e31
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2021-04-05 14:07:25 +0200

    pl

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const path = require('path')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
